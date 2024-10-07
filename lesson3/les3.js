d71c510e7e343393ad102866a743d7964b0b4951
Rubick213 <school30.96@mail.ru>
2024-10-07 07:59:21 +0300

    а

--- lesson3/les3.js
+++ lesson3/les3.js
@@ -6,14 +6,13 @@ function vowel(str) {
     if (str === null) {
         return 'Отмена'
     }
-    if (str.trim() === '') {
-        return 'пустая строка'
-    }
-    let count = {}
-    let countLetters = 0
-    let res = ''
+    // if (str.trim() = '') {
+    //     return 0
+    // }
 
-    let stroke = str.toLowerCase().replaceAll(' ','')
+    let countLetters = 0
+    
+    let stroke = str.toLowerCase()
     let vowelArr = ['а','у','о','и','э','ы','я','ю','е','ё']
     
     //подсчет гласных букв
@@ -21,29 +20,11 @@ function vowel(str) {
         if (vowelArr.includes(stroke[i])) {
             // console.log(`${stroke[i]}: находиться в массиве гласных под индексом ${vowelArr.indexOf(stroke[i])}`);
             countLetters++
-
-            if (!(stroke[i] in count)) {
-                count[stroke[i]] = 1
-            } else {
-                count[stroke[i]]++
-            }
         }  
     }
     
-    // проверка ялвяется ли обьект пустой
-    if (Object.keys(count).length == 0) {
-        return 'В строке нет гласных'
-    } else {
-        let lettersEnd = countLetters > 1 ? 'гласных' : 'гласная'
-        
-        res = `в данной строке ${countLetters} ${lettersEnd} :\n`
-
-        for (let el in count) {
-            res += `${el}: ${count[el]} шт.\n`
-        }
-    }
     
-    return res
+    return countLetters
 }
 
 let vowelLetters = vowel(userPrompt);
